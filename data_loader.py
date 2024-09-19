b75410efd0fdf455d642c189894d0a96a823fa2f
JM <98459939+JM-mochi@users.noreply.github.com>
2024-09-19 10:51:11 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -15,7 +15,7 @@ from albumentations import HorizontalFlip, VerticalFlip, RandomRotate90, Compose
 
 imagenet_stats = [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
 data_dir1 = pathlib.Path("/content/drive/MyDrive/LANDSAT-8_jpg/jpg")
-data_dir2 = pathlib.Path("/content/drive/MyDrive/jpgtest2/jpg")
+data_dir2 = pathlib.Path("/content/drive/MyDrive/jpg_test2/jpg")
 TRAIN_DIR = data_dir1 / "train"
 VALID_DIR = data_dir2 / "val"
 
